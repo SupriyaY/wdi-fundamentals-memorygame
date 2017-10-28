dda5aff8ebae8d5f800a8dc828fe9222f94c8a57
Supriya Yerramilli <supriyayerramilli@Avinashs-MacBook-Pro.local>
2017-10-28 15:10:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardThree = "king";
-console.log("User flipped " + cardOne)
-console.log("User flipper " + cardThree)
+var cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+card0ne = cards[0];
+cardsInPlay.push(card0ne); {
+ console.log("User flipped queen");
+ }
+ cardTwo = cards[2];
+ cardsInPlay.push(cardTwo); {
+   console.log("User flipped king");
+ }
+
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");}
+else {
+alert("Sorry, try again!"); }
+ }
