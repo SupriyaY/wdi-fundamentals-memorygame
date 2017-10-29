8b5438f1a1d947e3f968674bb55a03f8119df22c
Supriya Yerramilli <supriyayerramilli@Avinashs-MacBook-Pro.local>
2017-10-29 01:03:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit:  "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 cardsInPlay = [];
 var checkForMatch = function () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -7,8 +28,12 @@ else {
 alert("Sorry, try again."); }
 };
 var flipCard = function (cardId) {
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank)
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage)
+cardsInPlay.push(cards[cardId].cardImage);
+console.log(cards[cardId].suit)
+cardsInPlay.push(cards[cardId].suit);
 checkForMatch();
 };
 flipCard(0);
